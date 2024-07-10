9f4e0d766bc9d405671b6eea2784ca80f8e226fe
Carlos <roldanpinerocarlos@gmail.com>
2024-07-10 14:07:03 +0200

    arch

--- install.sh
+++ install.sh
@@ -1,28 +1,36 @@
 # Install requirements
-which dnf &>/dev/null && sudo dnf install git stow zsh tmux fzf curl wget alacritty neovim
+# sudo dnf install git stow zsh tmux fzf curl wget alacritty neovim zoxide
+sudo pacman -S git stow zsh tmux fzf curl wget alacritty neovim zoxide
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
 
 ## zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth 1 https://github.com/unixorn/fzf-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-zsh-plugin
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+yay -S oh-my-posh
+#curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.local/bin
 mkdir -p $HOME/.local/share/fonts
-wget -P $HOME/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-wget -P $HOME/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-wget -P $HOME/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-wget -P $HOME/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
-fc-cache -v
+yay -S ttf-meslo-nerd-font-powerlevel10k
+#wget -P $HOME/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+#wget -P $HOME/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+#wget -P $HOME/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+#wget -P $HOME/.local/share/fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+#fc-cache -v
 
 ## alacritty
 mkdir -p ~/.config/alacritty/themes
-git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/
 
 ## tmux
 mkdir -p ~/.config/tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
 ## micromamba
-mkdir $HOME/Programs
-echo '' | PREFIX_LOCATION=$HOME/Programs/micromamba $SHELL <(curl -L micro.mamba.pm/install.sh)
+#mkdir $HOME/Programs
+#echo '' | PREFIX_LOCATION=$HOME/Programs/micromamba $SHELL <(curl -L micro.mamba.pm/install.sh)
+yay -S micromamba-bin
+micromamba shell init --shell zsh
 
 ## neovim
 git clone https://github.com/LazyVim/starter ~/.config/nvim
@@ -35,3 +43,5 @@ stow .
 
 # Change shell
 chsh -s /usr/bin/zsh
+
+echo "REMEMBER LOG OFF AND THEN RUN CRTL+i IN A NEW TERMINAL"
